758c31653dded73fd25c3625c5cb88ace6812bac
Spandana102 <Spandanaaedu3110@gmail.com>
2026-09-20 17:55:54 +0530

    Updated project files

--- server/config/db.js
+++ server/config/db.js
@@ -2,14 +2,26 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI);
+        await mongoose.connect(process.env.MONGO_URI, {
+            serverSelectionTimeoutMS: 10000
+        });
 
         console.log("MongoDB Connected Successfully");
 
     } catch (error) {
         console.log("MongoDB Connection Failed");
-        console.error(error);
-        process.exit(1);
+        console.log("Name:", error.name);
+        console.log("Message:", error.message);
+
+        if (error.reason && error.reason.servers) {
+            console.log("\nMongoDB Server Details:");
+
+            for (const [address, server] of error.reason.servers) {
+                console.log("\nServer:", address);
+                console.log("Type:", server.type);
+                console.log("Error:", server.error?.message || "No detailed error");
+            }
+        }
     }
 };
 
--- server/server.js
+++ server/server.js
@@ -1,5 +1,4 @@
-const dns = require("dns");
-dns.setServers(["8.8.8.8"]);
+
 
 const express = require("express");
 const cors = require("cors");
